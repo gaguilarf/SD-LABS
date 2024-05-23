1f9d75c2effb1a77c09b2fd1d836de8341124f35
Josué Mathías Miguel Cahuana Aguilar <90291980+josuecahuana@users.noreply.github.com>
2024-05-22 23:36:44 -0500

    Subido del ejercicio resuelto por el docente y el anexo1

---
+++ Lab 4/Ejercicio Resuelto/MedicineInterface.java
@@ -0,0 +1,9 @@
+package anexo1;
+
+import java.rmi.Remote;
+
+public interface MedicineInterface extends Remote {
+	public Medicine getMedicine( int amount ) throws Exception;
+	public int getStock() throws Exception;
+	public String print() throws Exception;
+}
---
+++ Lab 4/Ejercicio Resuelto/StockException.java
@@ -0,0 +1,7 @@
+package anexo1;
+
+public class StockException extends Exception{
+	public StockException(String msg) {
+		super(msg);
+	}
+}
